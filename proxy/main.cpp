afc95b670a35a6cd5cdf9bf8ded249c6a98f85f9
Alireza67 <Alirezaabbasim@gmail.com>
2024-01-27 21:05:13 +0330

    [UPD] use std::unique_ptr instead of std::shared_ptr

--- proxy/main.cpp
+++ proxy/main.cpp
@@ -35,22 +35,28 @@ void Print(string msg)
 	cout << msg << endl;
 }
 
-void ProxySteerable(shared_ptr<void> context, vector<string> publisherAddresses, string proxyPublisherAddress, string captureAddress, string controlAddress)
+using UniquePtrWithCustomDelete = unique_ptr<void, void(*)(void*)>;
+
+void ProxySteerable(UniquePtrWithCustomDelete* context,
+	vector<string> publishersAddresses, 
+	string proxyPublisherAddress, 
+	string captureAddress, 
+	string controlAddress)
 {
-	auto xsub = zmq_socket(context.get(), ZMQ_XSUB);
-	for (auto& item : publisherAddresses)
+	auto xsub = zmq_socket(context->get(), ZMQ_XSUB);
+	for (auto& item : publishersAddresses)
 	{
 		auto res = zmq_connect(xsub, item.c_str());
 	}
 
-	auto xpub = zmq_socket(context.get(), ZMQ_XPUB);
+	auto xpub = zmq_socket(context->get(), ZMQ_XPUB);
 	auto res = zmq_bind(xpub, proxyPublisherAddress.c_str());
 
-	auto capture = zmq_socket(context.get(), ZMQ_PUB);
+	auto capture = zmq_socket(context->get(), ZMQ_PUB);
 	res = zmq_bind(capture, captureAddress.c_str());
 
-	auto control = zmq_socket(context.get(), ZMQ_PULL);
-	res = zmq_connect(control, controlAddress.c_str());
+	auto control = zmq_socket(context->get(), ZMQ_PULL);
+	res = zmq_bind(control, controlAddress.c_str());
 	auto timeout = 1000;
 	res = zmq_setsockopt(control, ZMQ_RCVTIMEO, &timeout, sizeof(timeout));
 
@@ -69,15 +75,15 @@ void ProxySteerable(shared_ptr<void> context, vector<string> publisherAddresses,
 	res = zmq_close(control);
 }
 
-void Proxy(shared_ptr<void> context, vector<string> publisherAddresses, string proxyPublisherAddress, string captureAddress)
+void Proxy(UniquePtrWithCustomDelete* context, vector<string> publisherAddresses, string proxyPublisherAddress, string captureAddress)
 {
-	auto capture = zmq_socket(context.get(), ZMQ_PUB);
+	auto capture = zmq_socket(context->get(), ZMQ_PUB);
 	auto res = zmq_bind(capture, captureAddress.c_str());
 
-	auto xpub = zmq_socket(context.get(), ZMQ_XPUB);
+	auto xpub = zmq_socket(context->get(), ZMQ_XPUB);
 	res = zmq_bind(xpub, proxyPublisherAddress.c_str());
 
-	auto xsub = zmq_socket(context.get(), ZMQ_XSUB);
+	auto xsub = zmq_socket(context->get(), ZMQ_XSUB);
 	for (auto& item : publisherAddresses)
 	{
 		res = zmq_connect(xsub, item.c_str());
@@ -95,9 +101,9 @@ void Proxy(shared_ptr<void> context, vector<string> publisherAddresses, string p
 	res = zmq_close(capture);
 }
 
-void Capture(shared_ptr<void> context, string captureAddress, vector<int> filters)
+void Capture(UniquePtrWithCustomDelete* context, string captureAddress, vector<int> filters)
 {
-	auto receiver = zmq_socket(context.get(), ZMQ_SUB);
+	auto receiver = zmq_socket(context->get(), ZMQ_SUB);
 	auto res = zmq_connect(receiver, captureAddress.c_str());
 
 	for (auto& item : filters)
@@ -127,10 +133,10 @@ void Capture(shared_ptr<void> context, string captureAddress, vector<int> filter
 	res = zmq_close(receiver);
 }
 
-void Control(shared_ptr<void> context, string controlAddress)
+void Control(UniquePtrWithCustomDelete* context, string controlAddress)
 {
-	auto sender = zmq_socket(context.get(), ZMQ_PUSH);
-	auto res = zmq_bind(sender, controlAddress.c_str());
+	auto sender = zmq_socket(context->get(), ZMQ_PUSH);
+	auto res = zmq_connect(sender, controlAddress.c_str());
 
 	uint16_t temp;
 
@@ -151,9 +157,9 @@ void Control(shared_ptr<void> context, string controlAddress)
 	res = zmq_close(sender);
 }
 
-void Publisher(shared_ptr<void> context, string name, string address, int filter, int message)
+void Publisher(UniquePtrWithCustomDelete* context, string name, string address, int filter, int message)
 {
-	auto socketSender = zmq_socket(context.get(), ZMQ_PUB);
+	auto socketSender = zmq_socket(context->get(), ZMQ_PUB);
 	auto res = zmq_bind(socketSender, address.c_str());
 
 	while (kLiveFlag.load())
@@ -169,9 +175,9 @@ void Publisher(shared_ptr<void> context, string name, string address, int filter
 	res = zmq_close(socketSender);
 }
 
-void Subscriber(shared_ptr<void> context, string name, string ProxyAddress, int filter)
+void Subscriber(UniquePtrWithCustomDelete* context, string name, string ProxyAddress, int filter)
 {
-	auto socketReceiver = zmq_socket(context.get(), ZMQ_SUB);
+	auto socketReceiver = zmq_socket(context->get(), ZMQ_SUB);
 	auto res = zmq_connect(socketReceiver, ProxyAddress.c_str());
 	res = zmq_setsockopt(socketReceiver, ZMQ_SUBSCRIBE, &filter, sizeof(filter));
 	auto timeout = 1000;
@@ -203,15 +209,13 @@ void ResolveSlowJoinerSyndrome()
 
 int main()
 {
-	shared_ptr<void> context{nullptr};
-	{
-		shared_ptr<void> temp(zmq_ctx_new(), [](void* ctx) {
+
+	unique_ptr<void, void(*)(void*)> context(zmq_ctx_new(), [](void* ctx) 
+		{
 			auto res = zmq_ctx_shutdown(ctx);
 			res = zmq_ctx_destroy(ctx);
-			});
-		context = temp;
-	}
-	
+		});
+
 
 	auto publisherPort = 9000;
 	auto publisherAddress = "inproc://job_1";
@@ -219,7 +223,7 @@ int main()
 	auto publisherPort2 = 9001;
 	auto publisherAddress2 = "inproc://job_2";
 
-	auto publisherAddresses = vector<string>{ publisherAddress, publisherAddress2 };
+	auto publishersAddresses = vector<string>{ publisherAddress, publisherAddress2 };
 
 	auto proxyPublisherPort = 10000;
 	auto proxyPublisherAddress = "tcp://127.0.0.1:"s + to_string(proxyPublisherPort);
@@ -230,15 +234,14 @@ int main()
 	auto filter1 = 66;
 	auto filter2 = 77;
 	auto filters = vector<int>{ filter1, filter2 };
-
-	auto proxy = thread(ProxySteerable, context, publisherAddresses, proxyPublisherAddress, captureAddress, controlAddress);
+	auto proxy = thread(ProxySteerable, &context, publishersAddresses, proxyPublisherAddress, captureAddress, controlAddress);
 	ResolveSlowJoinerSyndrome();
-	auto pub2 = thread(Publisher, context, "pub2"s, publisherAddress2, filter2, 0);
-	auto pub1 = thread(Publisher, context, "pub1"s, publisherAddress, filter1, 100);
-	auto sub1 = thread(Subscriber, context, "sub1"s, proxyPublisherAddress, 66);
-	auto sub2 = thread(Subscriber, context, "sub2"s, proxyPublisherAddress, 77);
-	auto capture = thread(Capture, context, captureAddress, filters);
-	auto control = thread(Control, context, controlAddress);
+	auto pub2 = thread(Publisher, &context, "pub2"s, publisherAddress2, filter2, 0);
+	auto pub1 = thread(Publisher, &context, "pub1"s, publisherAddress, filter1, 100);
+	auto sub1 = thread(Subscriber, &context, "sub1"s, proxyPublisherAddress, 66);
+	auto sub2 = thread(Subscriber, &context, "sub2"s, proxyPublisherAddress, 77);
+	auto capture = thread(Capture, &context, captureAddress, filters);
+	auto control = thread(Control, &context, controlAddress);
 
 	while (kLiveFlag.load())
 	{
